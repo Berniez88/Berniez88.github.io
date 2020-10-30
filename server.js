407c059f30061d20f8858451691623c27895b294
bzhong88 <bzhong88@users.noreply.github.com>
2020-10-30 01:35:39 -0400

    updates

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import countries from './public/lab_6/countries.js';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -25,10 +26,13 @@ app.route('/api')
     console.log('GET request detected');
     res.send(`Lab 5 for Bernie Zhong`);
   })
-  .post((req, res) => {
+  .post(async(req, res) => {
+    const data = await fetch("https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json")
+    const json = await data.json();
+    res.json(json);
     res.json(countries);
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
+    console.log('fetch request data', data);
   });
 
 app.listen(port, () => {
